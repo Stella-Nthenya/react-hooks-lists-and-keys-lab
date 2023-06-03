8e0a5608adaf44d56af7d6bba117c5d93edc15d2
Stella Nthenya <stellaalogi@gmail.com>
2023-06-03 23:09:34 +0300

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,21 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((projects)=> 
+          <ProjectItem key={projects.id} 
+            name={projects.name} 
+            technologies={projects.technologies}
+            about={projects.about}>
+          </ProjectItem>)}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+
